81222b190d12ac71095a9dc6022fe5d862f89ad5
Avery Aaron <business.loveai@gmail.com>
2022-09-05 19:48:26 -0500

    Added example explanation

--- src/game/gm_client.cpp
+++ src/game/gm_client.cpp
@@ -87,7 +87,9 @@ namespace game {
                 lag -= Game::MS_PER_TICK;
             }
 
-            // Render at position between ticks (input is percentage of next tick)
+            // Render object positions between ticks (input is percentage of next tick)
+            // Example: Bullet is on left of screen on tick 1, and right on tick two, but render happens
+            // at tick 1.5. Input is 0.5, meaning the bullet should render in the middle of the screen.
             gameState->render(lag / Game::MS_PER_TICK);
         }
 
