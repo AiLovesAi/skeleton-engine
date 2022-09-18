284511668cd87849221ea66e80332a136710a0a9
Avery Aaron <business.loveai@gmail.com>
2022-09-18 16:21:22 -0500

    Switched to CMake

--- build.bat
+++ build.bat
@@ -1,21 +1,17 @@
+@echo off
+set BUILD_TYPE="RELEASE"
+
 :: Compile shaders
 for %%F in (shaders/*) do (
   glslc "shaders\%%F" -o "assets\shaders\%%F.spv"
 )
 
-:: Compile binary resources for icon and info
-windres resources.rc -o resources.o
-
 :: Compile code and link libraries (-mwindows for no console)
-g++^
- -Wall -Wno-volatile -pipe -Ofast -Os -m64 -std=c++23^
- -mwindows -static-libgcc -static-libstdc++^
- -DNDEBUG^
- src/*.cpp src/util/*.cpp src/graphics/systems/*.cpp src/graphics/window/*.cpp src/graphics/vulkan/*.cpp src/graphics/game/*.cpp src/game/sides/*.cpp src/game/world/*.cpp src/game/states/*.cpp src/game/components/*.cpp src/input/*.cpp src/audio/*.cpp^
- -o out.exe resources.o^
- -I lib/include -L lib/^
- -Wl,--export-all-symbol^
- -Wl,-Bstatic -lstdc++ -lpthread -l:libglfw3.a -l:vulkan-1.lib^
- -Wl,-Bdynamic -lgdi32 -lversion -lsoft_oal -lsndfile
+cmake ^
+ -G "MinGW Makefiles" ^
+ -DCMAKE_CXX_COMPILER=g++ ^
+ -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
+ .
+mingw32-make
 
 PAUSE
