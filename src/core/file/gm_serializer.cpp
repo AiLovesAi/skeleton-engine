d1bf4fcd90a02125bec2852250a4721da0c6da77
Avery Aaron <business.loveai@gmail.com>
2022-10-02 11:24:54 -0500

    Revert "Serializer stores bits"
    This reverts commit 2fed14f8e6861be3b23e54ba765f752da8cabddb.

--- src/core/file/gm_serializer.cpp
+++ src/core/file/gm_serializer.cpp
@@ -1,44 +1,24 @@
 #include "gm_serializer.hpp"
 
 #include <algorithm>
+#include <cstring>
 
 namespace game {
-    void Serializer::writeBits(const uint8_t data, const size_t count) {
-        size_t resultHead = head_ + count;
-
-        if (resultHead > capacity_) {
-            resizeBuffer(std::max((capacity_ >> 3) * 2, (resultHead >> 3) + !!(resultHead & 0x7)));
-        }
-
-        size_t byteOffset = head_ >> 3; // Number of fully written bytes
-        uint8_t bitOffset = head_ & 0x7; // Bit remainder
-
-        // Preserve remaining bits
-        uint8_t mask = ~(0xff << bitOffset);
-        buffer_[byteOffset] = (buffer_[byteOffset] & mask) | (data << bitOffset);
-
-        uint8_t freeBits = 0x8 - bitOffset; // Bits remaining in current byte
-        // Use another byte if needed
-        if (freeBits < count) {
-            buffer_[byteOffset + 1] = data >> freeBits;
-        }
-
-        head_ = resultHead;
+    void Serializer::resizeBuffer(const size_t size) {
+        buffer_ = static_cast<char*>(std::realloc(buffer_, size));
+        capacity_ = size;
     }
 
-    void Serializer::writeBits(const void* data, size_t count) {
-        const char* srcByte = static_cast<const char*>(data);
+    void Serializer::write(const void* data, const size_t size) {
+        // TODO Translate to network endianness
+        size_t resultHead = head_ + size;
 
-        // Write full bytes
-        while (count > 8) {
-            writeBits(*srcByte, 8);
-            ++srcByte;
-            count -= 8;
+        if (resultHead > capacity_) {
+            resizeBuffer(std::max(capacity_ * 2, resultHead));
         }
-
-        // Write remaining bits
-        if (count > 0) {
-            writeBits(*srcByte, count);
-        }
+        
+        // TODO Write bits
+        std::memcpy(buffer_ + head_, data, size);
+        head_ = resultHead;
     }
 }
--- src/core/file/gm_serializer.hpp
+++ src/core/file/gm_serializer.hpp
@@ -1,48 +1,31 @@
 #pragma once
 
 #include <cstdint>
-#include <cstring>
-#include <memory>
 #include <type_traits>
 
 namespace game {
     class Serializer {
         public:
             // Constructors
-            Serializer() { resizeBuffer(64); }
-            ~Serializer() { std::free(buffer_); }
+            Serializer();
+            ~Serializer();
 
             // Functions
-            void reset() { head_ = 0; }
+            void write(const void* data, const size_t size);
 
-            void writeBits(const uint8_t data, const size_t count);
-            void writeBits(const void* data, size_t count);
-            void writeBits(const bool data) { writeBits(static_cast<uint8_t>(data), 1); }
-            template<typename T>
-            void writeBits(const T data) {
-                static_assert(std::is_arithmetic<T>::value || std::is_enum<T>::value,
-                    "Generic write only supports primitive data types.");
-                writeBits(&data, sizeof(T) * 8);
-            }
-
-            void writeBytes(const void* data, const size_t count) { writeBits(data, count << 3); }
             template<typename T>
             void write(const T data) {
                 static_assert(std::is_arithmetic<T>::value || std::is_enum<T>::value,
                     "Generic write only supports primitive data types.");
-                writeBytes(&data, sizeof(T));
+                write(&data, sizeof(data));
             }
         
         private:
             // Functions
-            void resizeBuffer(const size_t size) {
-                buffer_ = static_cast<char*>(std::realloc(buffer_, size));
-                capacity_ = size;
-            }
+            void resizeBuffer(size_t size);
 
             // Variables
             char* buffer_ = nullptr;
-            // Note: Measured in bits.
             size_t head_ = 0;
             size_t capacity_ = 0;
     };
