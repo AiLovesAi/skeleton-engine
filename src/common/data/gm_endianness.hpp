f17be2797a1d22a8251aec0fb2e6a65bcbfd764f
Avery Aaron <business.loveai@gmail.com>
2022-10-10 18:13:12 -0500

    Fixed ntoh function

--- src/common/data/gm_endianness.hpp
+++ src/common/data/gm_endianness.hpp
@@ -24,7 +24,7 @@ namespace game {
             }
             template <typename T>
             static inline T ntoh(const T data) {
-                hton(data);
+                return hton(data);
             }
             
         private:
