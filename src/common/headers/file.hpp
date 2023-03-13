e2f1a7160245254291f2756bb010f745e2752bcf
Avery Aaron <AiLovesAi@users.noreply.github.com>
2023-03-12 23:42:18 -0500

    Added common header files

---
+++ src/common/headers/file.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../data/file/gm_compression.hpp"
+#include "../data/file/gm_file.hpp"
+#include "../data/file/gm_logger.hpp"
---
+++ src/common/headers/string.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../data/string/gm_string.hpp"
+#include "../data/string/gm_format_string.hpp"
+#include "../data/string/gm_utf8.hpp"
