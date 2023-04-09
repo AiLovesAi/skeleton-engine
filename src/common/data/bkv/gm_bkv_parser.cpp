5e57ee537794c15d1906350291a940fd28ea856e
Avery Aaron <business.loveai@gmail.com>
2023-04-09 12:35:20 -0500

    Renamed BKV_Buffer.cpp to BKV_Parser.cpp
    No idea why this was called BKV_Buffer.cpp in the first place when it was clearly the source of BKV_Parser.hpp

--- src/common/data/bkv/gm_bkv_parser.cpp
+++ src/common/data/bkv/gm_bkv_parser.cpp
@@ -9,6 +9,7 @@
 
 namespace game {
     void BKV_Parser::openCompound() {
+        // Input validation
         if (_tag & BKV::BKV_ARRAY) {
             UTF8Str msg = FormatString::formatString("Compound in unclosed BKV array at %ld", _charactersRead);
             throw std::runtime_error(msg.get());
@@ -21,8 +22,7 @@ namespace game {
         _buffer._bkv[_buffer._tagHead] = BKV::BKV_COMPOUND;
         if (_buffer._tagHead == _buffer._head) { // This will be true for the opening compound
             _buffer._head++;
-            _buffer._bkv[_buffer._head] = '\0'; // 0 for name length
-            _buffer._head++;
+            _buffer._bkv[_buffer._head++] = 0; // 0 for name length
         }
         _depth.push(_buffer._head);
         _buffer._head += BKV::BKV_COMPOUND_SIZE;
@@ -30,7 +30,7 @@ namespace game {
         _buffer._tagHead = _buffer._head;
         
         if (_depth.size() > BKV::BKV_COMPOUND_DEPTH_MAX) {
-            UTF8Str msg = FormatString::formatString("Reached maximum compound depth in SBKV at index %ld: %lu/%lu.",
+            UTF8Str msg = FormatString::formatString("Reached maximum compound depth in SBKV at index %ld: %lu/%u",
                 _charactersRead, _depth.size(), BKV::BKV_COMPOUND_DEPTH_MAX
             );
             throw std::runtime_error(msg.get());
@@ -41,17 +41,16 @@ namespace game {
         try {
             StringBuffer::checkResize(_buffer._bkv, _buffer._head + 1, _buffer._head, _buffer._capacity);
         } catch (std::runtime_error &e) { throw; }
-        _buffer._bkv[_buffer._head] = BKV::BKV_END;
-        _buffer._head++;
+        _buffer._bkv[_buffer._head++] = BKV::BKV_END;
 
         int64_t size = _buffer._head - _depth.top() + sizeof(uint32_t);
         if ((size <= 0) || (size > BKV::BKV_COMPOUND_MAX)) {
-            UTF8Str msg = FormatString::formatString("BKV compound bigger than maximum size at index %d: %ld/%ld",
+            UTF8Str msg = FormatString::formatString("BKV compound bigger than maximum size at index %d: %ld/%u",
                 _depth.top(), size, BKV::BKV_COMPOUND_MAX
             );
             throw std::runtime_error(msg.get());
         }
-        uint32_t len = Endianness::hton(static_cast<uint32_t>(size));
+        const uint32_t len = Endianness::hton(static_cast<uint32_t>(size));
         std::memcpy(_buffer._bkv + _depth.top(), &len, BKV::BKV_COMPOUND_SIZE);
         _depth.pop();
         _buffer._valHead = _buffer._head;
@@ -71,6 +70,7 @@ namespace game {
     void BKV_Parser::endKV(const char c) {
         _buffer._valHead = _buffer._head;
         if (_tag & BKV::BKV_ARRAY) {
+            // Input validation
             if (c == '}') {
                 UTF8Str msg = FormatString::formatString("Closing character is '}' when in SBKV array at index: %ld", _charactersRead);
                 throw std::runtime_error(msg.get());
@@ -88,6 +88,7 @@ namespace game {
             _stateTree.push(&_arrayState);
             try { state()->parse(*this, c); } catch (std::runtime_error &e) { throw; }
         } else {
+            // Input validation
             if (c == ']') {
                 UTF8Str msg = FormatString::formatString("Closing character is ']' when not in SBKV array at index: %ld",
                     _charactersRead
