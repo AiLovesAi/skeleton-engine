149e37f7ce6e894b828128dcea7426ce5d835b81
Avery Aaron <business.loveai@gmail.com>
2023-04-08 22:15:08 -0500

    Corrected copy constructor for UTF8Str

--- src/common/data/string/gm_utf8.hpp
+++ src/common/data/string/gm_utf8.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <memory>
+#include <cstring>
 
 namespace game {
     typedef struct UTF8Str_ {
@@ -13,6 +14,21 @@ namespace game {
             UTF8Str_() : _len{0}, _str{nullptr} {}
             UTF8Str_(const int64_t len, const std::shared_ptr<const char> str) :
                 _len{len}, _str{str} {}
+            UTF8Str_(const UTF8Str_& str) : _len{str.length()} {
+                char* copy = static_cast<char*>(std::malloc(_len + 1));
+                std::memcpy(copy, str.get(), _len);
+                copy[_len] = '\0';
+                _str = std::shared_ptr<const char>(copy, std::free);
+            }
+            
+            UTF8Str_& operator=(const UTF8Str_& str) {
+                _len = str.length();
+                char* copy = static_cast<char*>(std::malloc(_len + 1));
+                std::memcpy(copy, str.get(), _len);
+                copy[_len] = '\0';
+                _str = std::shared_ptr<const char>(copy, std::free);
+                return *this;
+            }
 
             inline const char* get() const {
                 return _str.get();
@@ -22,4 +38,5 @@ namespace game {
                 return _len;
             }
     } UTF8Str;
+    #define EMPTY_STR UTF8Str{sizeof("NULL") - 1, std::shared_ptr<const char>("NULL", [](const char*){})}
 }
